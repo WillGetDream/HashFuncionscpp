806d07a0b7c7c859c4c818039ba1fc90ca4e5e01
william <williamcodingx@gmail.com>
2019-10-20 00:59:57 -0700

    godd

---
+++ HashFuncionscpp/cmake-build-debug/CMakeFiles/HashFuncionscpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HashFuncionscpp.dir/main.cpp.o"
+  "CMakeFiles/HashFuncionscpp.dir/GeneralHashFunctions.cpp.o"
+  "HashFuncionscpp.pdb"
+  "HashFuncionscpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HashFuncionscpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
